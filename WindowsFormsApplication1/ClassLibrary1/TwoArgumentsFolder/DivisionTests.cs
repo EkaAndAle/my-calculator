8cea6d30664812d10c5573a13c1beacbb2379013
Kaparushkin Andrey <akaparushkin99@mail.ru>
2018-07-04 12:55:17 +0700

    calculator simple tests

---
+++ WindowsFormsApplication1/ClassLibrary1/TwoArgumentsFolder/DivisionTests.cs
@@ -0,0 +1,19 @@
+﻿using WindowsFormsApplication1.TwoArgumentsFolder;
+using NUnit.Framework;
+
+namespace ClassLibrary1.TwoArgumentsFolder
+{
+    public class DivisionTests
+    {
+        [Test]
+        public void Division()
+        {
+            ITwoArgumentsCalculator calculator = new Division();
+            double result = calculator.Calculate(10,2);
+            // подготовительные действия
+            Assert.AreEqual(5, result);
+        }
+
+    }
+
+}
---
+++ WindowsFormsApplication1/ClassLibrary1/TwoArgumentsFolder/PlusTests.cs
@@ -0,0 +1,19 @@
+﻿using WindowsFormsApplication1.TwoArgumentsFolder;
+using NUnit.Framework;
+
+namespace ClassLibrary1.TwoArgumentsFolder
+{
+    public class PlusTests
+    {
+        [Test]
+        public void Plus()
+        {
+            ITwoArgumentsCalculator calculator = new Plus();
+            double result = calculator.Calculate(8, 7);
+            // подготовительные действия
+            Assert.AreEqual(15, result);
+        }
+
+    }
+
+}
